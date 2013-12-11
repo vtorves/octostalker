fa83d259d0e81045d302b7c5fe28ceffa1911a7d
Arthur Neves <arthurnn@gmail.com>
2013-12-10 20:55:08 -0500

    Test pagination

--- follow.rb
+++ follow.rb
@@ -1,10 +1,20 @@
 require 'octokit'
 token = ENV['GITHUB_TOKEN']
+
+stack = Faraday::Builder.new do |builder|
+  builder.response :logger
+  builder.use Octokit::Response::RaiseError
+  builder.adapter Faraday.default_adapter
+end
+Octokit.middleware = stack
+
 client = Octokit::Client.new(access_token: token)
 client.configure do |config|
-  config.per_page = 100
-  config.auto_paginate = true
+  config.per_page = 16
+  config.auto_paginate = false
 end
+
+p client.organization_members('Shopify', per_page: 16, auto_paginate: false).count
 
 #followers = client.followers.map(&:login)
 #p followers
