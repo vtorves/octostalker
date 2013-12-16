9db2a3be9710a2c6c6b42a41a0fdb6728660c613
Arthur Neves <arthurnn@gmail.com>
2013-12-15 21:06:20 -0500

    Add check to avoid :bomb:

--- octokit_ext.rb
+++ octokit_ext.rb
@@ -4,7 +4,9 @@ module Octokit
 
       def organization_members_size(org, options = {})
         organization_members(org, per_page: 1, auto_paginate: false)
-        @last_response.rels[:last].href_template.expand({}).query_values["page"].to_i
+        if link = @last_response.rels[:last]
+          link.href_template.expand({}).query_values["page"].to_i
+        end
       end
     end
   end
